99c4dff522c0bfd990826644102149fd404680c4
Nikhil Yadav <nickelyadav57@gmail.com>
2021-06-28 11:08:53 +0530

    Setup created and database connected

---
+++ Memories App/server/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+
+const app = express();
+
+app.use(express.urlencoded({limit: "30mb", extended: true}));
+app.use(express.json({limit: "30mb", extended: true}));
+
+const CONNECTION_URL = 'mongodb+srv://mitnick:gHyoPJeTtmdk1zay@nick-cluster.u7ruw.mongodb.net/test?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
