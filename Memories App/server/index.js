86f7a57457e6368d66db022c2f19edb8627e83af
Nikhil Yadav <nickelyadav57@gmail.com>
2021-07-04 20:00:31 +0530

    Middleware setup and database link

--- Memories App/server/index.js
+++ Memories App/server/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import postRoute from './routes/posts.js';
 
 const app = express();
+
+app.use('/posts', postRoute);
 
 app.use(express.urlencoded({limit: "30mb", extended: true}));
 app.use(express.json({limit: "30mb", extended: true}));
@@ -11,8 +13,9 @@ app.use(express.json({limit: "30mb", extended: true}));
 const CONNECTION_URL = 'mongodb+srv://mitnick:gHyoPJeTtmdk1zay@nick-cluster.u7ruw.mongodb.net/test?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-    .catch((error) => console.log(error.message));
+// mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+//     .catch((error) => console.log(error.message));
 
-mongoose.set('useFindAndModify', false);
+// mongoose.set('useFindAndModify', false);
+app.listen(5000);
