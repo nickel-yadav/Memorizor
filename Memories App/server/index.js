de958696fbb36e5f15172bcf2c08a0f84b41985b
Nikhil Yadav <nickelyadav57@gmail.com>
2021-07-06 20:22:23 +0530

    MVC server logic implemented for post creation

--- Memories App/server/index.js
+++ Memories App/server/index.js
@@ -13,9 +13,8 @@ app.use(express.json({limit: "30mb", extended: true}));
 const CONNECTION_URL = 'mongodb+srv://mitnick:gHyoPJeTtmdk1zay@nick-cluster.u7ruw.mongodb.net/test?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-// mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-//     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-//     .catch((error) => console.log(error.message));
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
 
-// mongoose.set('useFindAndModify', false);
-app.listen(5000);
+mongoose.set('useFindAndModify', false);
--- Memories App/server/routes/posts.js
+++ Memories App/server/routes/posts.js
@@ -5,4 +5,5 @@ const router = express.Router();
 
 router.get('/', getPosts);
 router.post('/', createPost);
+
 export default router;
