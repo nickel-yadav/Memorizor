b11481b6f51918cf9c1672ce6bcefbe2aafaa766
Nikhil Yadav <nickelyadav57@gmail.com>
2021-07-07 20:12:39 +0530

    components setup and directory structure arranged

--- Memories App/client/src/App.js
+++ Memories App/client/src/App.js
@@ -1,10 +1,29 @@
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import React from 'react';
+import memories from './images/memories.jpg';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
 
 const App = () =>  {
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+        <Container maxWidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center">Memories</Typography>
+                <img src={memories} alt="memories" height="60" width="60"/>
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     );
 }
 
