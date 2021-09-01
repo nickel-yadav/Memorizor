d3c99ff90e804d19f0a05f1b41e42331359f459b
Nikhil <60028530+nickel-yadav@users.noreply.github.com>
2021-09-01 19:46:07 +0530

    :truck: changes db uri

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(cors());
 
 app.use('/posts', postRoute);
 
-const CONNECTION_URL = 'mongodb+srv://mitnick:gHyoPJeTtmdk1zay@nick-cluster.u7ruw.mongodb.net/test?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://mitnick:gHyoPJeTtmdk1zay@sandbox.u7ruw.mongodb.net/memorizor?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
