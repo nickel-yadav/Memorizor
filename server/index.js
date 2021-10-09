20d52ad1527a538407e74656c8dadd43ad49a977
Nikhil <60028530+nickel-yadav@users.noreply.github.com>
2021-10-09 13:42:17 +0530

    deployement fixes

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import dotenv from 'dotenv';
 import postRoute from './routes/posts.js';
+import dotenv from 'dotenv';
 
 const app = express();
 dotenv.config();
@@ -14,12 +14,12 @@ app.use(cors());
 app.use('/posts', postRoute);
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the Memorizor');
-})
+    res.send('Memorizor API running');
+});
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
