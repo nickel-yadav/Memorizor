0800df97e04c65088b3335cb102bd248feb4b055
Nikhil <60028530+nickel-yadav@users.noreply.github.com>
2021-10-09 12:36:17 +0530

    :rocket: deploys application

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,10 +1,18 @@
+import { FETCH_ALL, CREATE, UPDATE, DELETE } from '../constants/actionTypes';
+
 export default (posts = [], action) => {
     switch(action.type) {
-        case 'FETCH ALL': {
+        case FETCH_ALL: {
             return action.payload;
         }
-        case 'CREATE': {
+        case CREATE: {
             return [...posts, action.payload]
+        }
+        case UPDATE: {
+             return posts.map((post) => post.id === action.payload._id ? action.payload: post);
+        }
+        case DELETE: {
+            return posts.filter((post) => post.id !== action.payload);
         }
         default: 
             return posts;
--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import postRoute from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 app.use(express.urlencoded({limit: "30mb", extended: true}));
 app.use(express.json({limit: "30mb", extended: true}));
@@ -11,10 +13,13 @@ app.use(cors());
 
 app.use('/posts', postRoute);
 
-const CONNECTION_URL = 'mongodb+srv://mitnick:gHyoPJeTtmdk1zay@sandbox.u7ruw.mongodb.net/memorizor?retryWrites=true&w=majority';
+app.get('/', (req, res) => {
+    res.send('Welcome to the Memorizor');
+})
+
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
